8a0cc5bffbd1106159de23e1f66d2b53ddaa0d54
BigEngland <andrewgwardwell@gmail.com>
2022-12-07 15:34:53 -0500

    test 2

--- netlify/functions/cron.js
+++ netlify/functions/cron.js
@@ -1,13 +1,6 @@
-const axios = require('axios');
-
 export const handler = async (event) => {
-    const response = await axios({
-      method: 'get',
-      url: 'https://www.edx.org/page-data/learn/machine-learning/page-data.json',
-      responseType: 'json'
-    });
-    return {
-        statusCode: 200,
-        body: JSON.stringify(response)
-    }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello World" }),
+  };
 }
