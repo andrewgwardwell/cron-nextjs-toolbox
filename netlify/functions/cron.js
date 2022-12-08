0dbf3fbe1370b8cc13f8177d01c424d563a20983
BigEngland <andrewgwardwell@gmail.com>
2022-12-08 10:43:13 -0500

    removed space id

--- netlify/functions/cron.js
+++ netlify/functions/cron.js
@@ -1,7 +1,7 @@
 const contentful = require('contentful-management');
 // CFPAT-
 const ACCESS_TOKEN = process.env.CNTFL_ACCESS;
-const spaceId = 'ii9ehdcj88bc';
+const spaceId = process.env.SPACE_ID;
 const envId = 'andrew-test';
 
 export const handler = async (event) => {
@@ -16,7 +16,6 @@ export const handler = async (event) => {
 
     // Now that we have a space, we can get entries from that space
     const entries = await environment.getEntries({limit: 1, content_type:'learnPage'});
-    const entry = entries.items[0];
     const fixedEntry = {
       "sys": {
           "type": "Link",
@@ -24,8 +23,11 @@ export const handler = async (event) => {
           "id": "739Yao78oRwksRTUjMtJ6v"
       }
     };
-    entry.fields.footerModules["en-US"].splice(0, 0, fixedEntry);
-    const updated = await (await entry.update()).publish();
+    let updated;
+    entries.items.forEach(async (entry) => {
+      entry.fields.footerModules["en-US"].splice(0, 0, fixedEntry);
+      updated = await (await entry.update()).publish();
+    });
 
     return {
       statusCode: 200,
